620aa7edcba170f97390c2a2262b6bf209009bcd
Zain Noman Kamali <38238384+zainknoman@users.noreply.github.com>
2026-09-21 14:31:12 +0500

    Initial commit: add src/test/java/com/yourcompany/formbot/FormBotApplicationTests.java

---
+++ src/test/java/com/yourcompany/formbot/FormBotApplicationTests.java
@@ -0,0 +1,9 @@
+package com.yourcompany.formbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormBotApplicationTests {
+    @Test void contextLoads() {}
+}
