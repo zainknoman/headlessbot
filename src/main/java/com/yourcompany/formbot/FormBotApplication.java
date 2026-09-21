4b490472caf50462e7ccc35c4108a35343d22bc6
Zain Noman Kamali <38238384+zainknoman@users.noreply.github.com>
2026-09-21 14:30:33 +0500

    Initial commit: add src/main/java/com/yourcompany/formbot/FormBotApplication.java

---
+++ src/main/java/com/yourcompany/formbot/FormBotApplication.java
@@ -0,0 +1,11 @@
+package com.yourcompany.formbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormBotApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FormBotApplication.class, args);
+    }
+}
